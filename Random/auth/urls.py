037a5ea1b27c2b6e91f277212ade1d7302519140
SamNiky <niky.sam@ya.ru>
2021-02-15 16:14:59 +0300

    1. Make urls path; 2. Make initial function for render page.

--- Random/auth/urls.py
+++ Random/auth/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'auth'
 urlpatterns = [
-    path('login/', views.login, name='login'),
+    path('', views.login, name='login'),
     path('sign-up/', views.signUp, name='sign-up'),
     path('sign-out/', views.signOut, name='sign-out'),
 ]
