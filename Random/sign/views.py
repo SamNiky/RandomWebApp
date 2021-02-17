755d1fb39ae720137a639f72b812078b3b3f1f23
SamNiky <niky.sam@ya.ru>
2021-02-17 14:36:26 +0300

    Installed static files

--- Random/sign/views.py
+++ Random/sign/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("start page")
+    return render(request, 'sign/index.html')
 
 def login(request):
     return HttpResponse("login page")
